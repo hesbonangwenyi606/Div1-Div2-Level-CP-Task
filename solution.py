b4f22d525ef3c493d3be95f1e16b4a625c29ed17
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-12-09 13:12:56 +0300

    updated

--- solution.py
+++ solution.py
@@ -1,24 +1,17 @@
-import sys
-input = sys.stdin.readline
-
-n = int(input())
-a = list(map(int, input().split()))
-
-maxA = 10**6 + 1
-freq = [0] * maxA
-
-# Step 1: count frequency of each number
-for x in a:
-    freq[x] += 1
-
-res = 0
-# Step 2: iterate from largest g to smallest
-for g in range(maxA-1, 0, -1):
-    count = 0
-    # sum counts of all multiples of g
-    for multiple in range(g, maxA, g):
-        count += freq[multiple]
-    # Step 3: calculate weight
-    res = max(res, count * g)
-
-print(res)
+hesbonangwenyi@hesbonangwenyi-HP-ProBook-430-G5:~/Downloads/subsequence_weight_maximization$ for i in {1..5}; do     echo "Running test case $i:"                    # Prints which test case is running
+    python3 solution.py < test_cases/$i.in | diff - test_cases/$i.out         && echo "OK"         || echo "FAIL"; done
+Running test case 1:
+1c1
+< 9
+---
+> 12
+FAIL
+Running test case 2:
+OK
+Running test case 3:
+OK
+Running test case 4:
+OK
+Running test case 5:
+OK
+hesbonangwenyi@hesbonangwenyi-HP-ProBook-430-G5:~/Downloads/subsequence_weight_maximization$ 
